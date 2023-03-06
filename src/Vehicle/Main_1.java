22e0312684ef58df59a07f4dc7ac931893badc8d
nguyenthiyen1295 <nguyenthiyen1295@gmail.com>
2023-03-06 20:49:06 +0700

    Update bài tập: Bài tập quản lý xe.

--- src/Vehicle/Main_1.java
+++ src/Vehicle/Main_1.java
@@ -8,11 +8,11 @@ public class Main_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		tinh_thue xe_dap_1 = new xe_dap(12000);
+		tinh_thue xe_dap_1 = new xe_dap(1200);
 		xe_dap_1.displays_infor();
-		tinh_thue xe_may = new xe_may(20000);
+		tinh_thue xe_may = new xe_may(200000);
 		xe_may.displays_infor();
-		tinh_thue xe_tai = new xe_tai(30000);
+		tinh_thue xe_tai = new xe_tai(300000);
 		xe_tai.displays_infor();
 		tinh_thue xe_khach = new xe_khach(30000,10);
 		xe_khach.displays_infor();
